2e24b894e1f1922d7e7df1a762583166606ef581
edwilliams <edwilliamsmusic@gmail.com>
2013-10-16 15:39:36 +0100

    edit face url

--- js/content.js
+++ js/content.js
@@ -764,6 +764,13 @@ var renderHTML = {
     // },
 
     childBrowserFunctionFACE : function() {
+
+        var facebook_standard = "https://www.facebook.com/dialog/feed?" +
+            "app_id=145634995501895" +
+            "&display=popup" +
+            "&caption=An%20example%20caption" +
+            "&link=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fdialogs%2F" +
+            "&redirect_uri=https://developers.facebook.com/tools/explorer"; 
         var facebookInfo = {
             name: 'Pandora Dress Agency',
             caption: 'The Game App',
@@ -772,13 +779,14 @@ var renderHTML = {
         };
         var facebook_url = 
             "https://www.facebook.com/dialog/feed?" +
-            "&name=" + facebookInfo.name +
-            "&caption=" + facebookInfo.caption +
-            "&description=" + facebookInfo.description +
-            "&link=" + facebookInfo.link +
             "&app_id=491627374259881" +
             "&display=popup" +
-            "&next=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D25%23cb%3Df1b39d2e98%26origin%3Dhttp%253A%252F%252Fpamela.cognique.co.uk%252Ffa0a82d7c%26domain%3Dpamela.cognique.co.uk%26relation%3Dopener%26frame%3Df2d2a921b%26result%3D%2522xxRESULTTOKENxx%2522";      
+            "&caption=" + facebookInfo.caption +
+            "&link=" + facebookInfo.link +
+            "&description=" + facebookInfo.description +
+            "&name=" + facebookInfo.name +
+            "&redirect_uri=https://www.google.com/";
+            // "&next=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D25%23cb%3Df1b39d2e98%26origin%3Dhttp%253A%252F%252Fpamela.cognique.co.uk%252Ffa0a82d7c%26domain%3Dpamela.cognique.co.uk%26relation%3Dopener%26frame%3Df2d2a921b%26result%3D%2522xxRESULTTOKENxx%2522";      
       window.plugins.childBrowser.showWebPage(facebook_url, { showLocationBar: true });      
     },
 
