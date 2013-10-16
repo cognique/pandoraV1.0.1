2f783fc92c91005c420c7d8100569c15d7163761
edwilliams <edwilliamsmusic@gmail.com>
2013-10-16 16:43:41 +0100

    fb link medi

--- js/content.js
+++ js/content.js
@@ -780,9 +780,9 @@ var renderHTML = {
             "&caption=The%20Game%20App" +
             "&description=Description%20goes%20here" + 
             "&name=Pandora%20Dress%20Agency" +  
-            "&link=http%3A%2F%2Fwww.pandoradressagency.co.uk%2F";
-
-            //"&redirect_uri=https://developers.facebook.com/tools/explorer";
+            "&link=http%3A%2F%2Fwww.pandoradressagency.co.uk%2F" +
+            "&redirect_uri=https://developers.facebook.com/tools/explorer" +
+            "&next=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D25%23cb%3Df1b39d2e98%26origin%3Dhttp%253A%252F%252Fpamela.cognique.co.uk%252Ffa0a82d7c%26domain%3Dpamela.cognique.co.uk%26relation%3Dopener%26frame%3Df2d2a921b%26result%3D%2522xxRESULTTOKENxx%2522";
 
         var facebook_url = 
             "https://www.facebook.com/dialog/feed?" +
