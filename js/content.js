fbb85084e462c8e924c1de7638ae0af9e2a97951
edwilliams <edwilliamsmusic@gmail.com>
2013-10-16 10:36:29 +0100

    new fb url

--- js/content.js
+++ js/content.js
@@ -768,15 +768,22 @@ var renderHTML = {
             "&link=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fdialogs%2F" +
             "&redirect_uri=https://developers.facebook.com/tools/explorer";
 
-        var facebook_url4 = "https://www.facebook.com/dialog/feed?" +
-            "app_id=491627374259881" +
+        var facebook_url3 = "https://www.facebook.com/dialog/feed?" +
+            "&app_id=491627374259881" +
             "&display=popup" +
             "&caption=" + facebookInfo.caption +
-            "&link=" + facebookInfo.link;
+            "&link=" + facebookInfo.link +
+            "&redirect_uri=http://www.pandoradressagency.co.uk/";
+            // "name=" + facebookInfo.name +
+            // "&description=" + facebookInfo.description +
+            // "&e2e=%7B%7D" +
+            // "&locale=en_US" +
+            // "&sdk=joey" +
+            // "&next=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D25%23cb%3Df1b39d2e98%26origin%3Dhttp%253A%252F%252Fpamela.cognique.co.uk%252Ffa0a82d7c%26domain%3Dpamela.cognique.co.uk%26relation%3Dopener%26frame%3Df2d2a921b%26result%3D%2522xxRESULTTOKENxx%2522";
 
         var twitter_url = "https://twitter.com/intent/tweet?text=I%27ve%20been%20playing%20the%20Pandora%20App%20Game";
 
-        var facebookWindowOpen = "window.open('" + facebook_url + "', '_blank', 'location=yes')";
+        var facebookWindowOpen = "window.open('" + facebook_url3 + "', '_blank', 'location=yes')";
         var twitterWindowOpen = "window.open('" + twitter_url + "', '_blank', 'location=yes')";
 
         var html =
