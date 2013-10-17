934648b94c995179a950735fb0a9695cb5396364
edwilliams <edwilliamsmusic@gmail.com>
2013-10-17 09:29:53 +0100

    reintroduce facebookInfo data

--- js/content.js
+++ js/content.js
@@ -766,18 +766,18 @@ var renderHTML = {
     childBrowserFunctionFACE : function() {
 
         var facebookInfo = {
-            name: 'Pandora Dress Agency',
-            caption: 'The Game App',
-            description: 'Description goes here...',
-            link: 'http://www.pandoradressagency.co.uk/'
+            name: 'PandoraDressAgency',
+            caption: 'TheGameApp',
+            description: 'Descriptiongoeshere...',
+            link: 'https%3A%2F%2Fwww.pandoradressagency.co.uk%2F'
         };
 
         var facebook_url = 
             "https://www.facebook.com/dialog/feed?" +
-            "&name=Pandora&nbsp;Dress&nbsp;Agency" +
-            "&caption=The&nbsp;Game&nbsp;App" +
-            "&description=Description&nbsp;goes&nbsp;here..." +
-            "&link=https%3A%2F%2Fwww.pandoradressagency.co.uk%2F" +
+            "&name=" + facebookInfo.name +
+            "&caption=" + facebookInfo.caption +
+            "&description=" + facebookInfo.description +
+            "&link=" + facebookInfo.link +
             "&app_id=491627374259881" +
             "&display=popup" +
             "&next=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D25%23cb%3Df1b39d2e98%26origin%3Dhttp%253A%252F%252Fpamela.cognique.co.uk%252Ffa0a82d7c%26domain%3Dpamela.cognique.co.uk%26relation%3Dopener%26frame%3Df2d2a921b%26result%3D%2522xxRESULTTOKENxx%2522";
